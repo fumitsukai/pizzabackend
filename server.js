41ec917354eced4bb79abb5c736fc9b56dec4bfd
fumitsukai <pandeleaalexandru321@gmail.com>
2024-03-08 14:14:06 +0000

    added files

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import pizzaRoutes from "./routes/pizzaRoutes.js";
+import cors from "cors";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api", pizzaRoutes);
+app.use(notFound);
+app.use(errorHandler);
+app.use(cors());
+
+app.all("/", (req, res) => res.send("Server is ready!"));
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
